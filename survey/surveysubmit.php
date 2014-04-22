97fc671121f386e69033bf217cbb5f05f9be5b6f
phaughian <patrick_haughian@hotmail.com>
2014-04-21 22:20:41 -0400

    final changes made
    site is now prepared for submission.

---
+++ survey/surveysubmit.php
@@ -0,0 +1,124 @@
+<?php 
+include "connect.php"; 
+//retrieve the current number of submissions and add one to it
+
+//$subs=
+
+//save all posts from template.php as php variables
+// $userID=$_POST['userID'];
+// $surveyID=$_POST['surveyID'];
+
+// $q1a=$_POST['1a'];
+// $q1b=$_POST['1b'];
+// $q1c=$_POST['1c'];
+// $q1d=$_POST['1d'];
+
+// $q2a=$_POST['2a'];
+// $q2b=$_POST['2b'];
+// $q2c=$_POST['2c'];
+// $q2d=$_POST['2d'];
+
+// $q3a=$_POST['3a'];
+// $q3b=$_POST['3b'];
+// $q3c=$_POST['3c'];
+// $q3d=$_POST['3d'];
+
+// $q4a=$_POST['4a'];
+// $q4b=$_POST['4b'];
+// $q4c=$_POST['4c'];
+// $q4d=$_POST['4d'];
+
+// $q5a=$_POST['5a'];
+// $q5b=$_POST['5b'];
+// $q5c=$_POST['5c'];
+// $q5d=$_POST['5d'];
+
+// $q6a=$_POST['6a'];
+// $q6b=$_POST['6b'];
+// $q6c=$_POST['6c'];
+// $q6d=$_POST['6d'];
+
+// $q7a=$_POST['7a'];
+// $q7b=$_POST['7b'];
+// $q7c=$_POST['7c'];
+// $q7d=$_POST['7d'];
+
+// $q8a=$_POST['8a'];
+// $q8b=$_POST['8b'];
+// $q8c=$_POST['8c'];
+// $q8d=$_POST['8d'];
+
+// $q9a=$_POST['9a'];
+// $q9b=$_POST['9b'];
+// $q9c=$_POST['9c'];
+// $q9d=$_POST['9d'];
+
+// $q10a=$_POST['10a'];
+// $q10b=$_POST['10b'];
+// $q10c=$_POST['10c'];
+// $q10d=$_POST['10d'];
+
+
+
+
+//concatinate a string together to submit to sql
+// $submitsurveysql="
+// UPDATE `survey`.`results` SET
+// `name` ='$name',
+
+// `q1a` ='$a1',
+// `q1b` ='$b1',
+// `q1c` ='$c1',
+// `q1d` ='$d1',
+
+// `q2a` ='$a2',
+// `q2b` ='$b2',
+// `q2c` ='$c2',
+// `q2d` ='$d2',
+
+// `q3a` ='$a3',
+// `q3b` ='$b3',
+// `q3c` ='$c3',
+// `q3d` ='$d3',
+
+// `q4a` ='$a4',
+// `q4b` ='$b4',
+// `q4c` ='$c4',
+// `q4d` ='$d4',
+
+// `q5a` ='$a5',
+// `q5b` ='$b5',
+// `q5c` ='$c5',
+// `q5d` ='$d5',
+
+// `q6a` ='$a6',
+// `q6b` ='$b6',
+// `q6c` ='$c6',
+// `q6d` ='$d6',
+
+// `q7a` ='$a7',
+// `q7b` ='$b7',
+// `q7c` ='$c7',
+// `q7d` ='$d7',
+
+// `q8a` ='$a8',
+// `q8b` ='$b8',
+// `q8c` ='$c8',
+// `q8d` ='$d8',
+
+// `q9a` ='$a9',
+// `q9b` ='$b9',
+// `q9c` ='$c9',
+// `q9d` ='$d9',
+
+// `q10a` ='$a10',
+// `q10b` ='$b10',
+// `q10c` ='$c10',
+// `q10d` ='$d10'
+// WHERE
+// `surveytemplate`.`SurveyTemplate_ID` ='$templateID'";
+
+								// mysql_query($submitsurveysql);
+								echo "Your answers has been saved. Thankyou <a href='account.php'> Return to account. </a>";
+
+?>
